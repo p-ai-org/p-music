e9e10f7d1e3c974f164291cbab1b47fa00600efb
Seohyeon Lee <96499556+lapcbws@users.noreply.github.com>
2022-10-21 17:17:10 -0700

    Update spotipy-albumsearch.py
    script to get dictionary (keys: first 100 album names in top 5000 dataset, values: list of songs in said album)

--- spotipy-albumsearch.py
+++ spotipy-albumsearch.py
@@ -1,3 +1,4 @@
+
 #create folders for each albums
 #use spotdl in command line to download all the songs in an album
 #import modules I need
@@ -17,15 +18,22 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id = cred.client_id, clien
     scope=scope))
 
 # find album by name
-# get external urls
+# get the first album uri
 
 album_names = album_data['Album']
-url_list = []
+album_dict = {}
 #need to skip rows that don't have needed values - how?
-for album_name in album_names:
+for album_name in album_names[:100]:
     results = sp.search(q = "album:" + album_name, type = "album")
     try:
-        album_url = results['albums']['items'][0]['external_urls']['spotify']
-    except: 
-        print("link doesn't exist")
-    url_list.append(album_url)
+        album_uri = results['albums']['items'][0]['uri']
+    except:
+        print("")
+    album_tracks_result = sp.album_tracks(album_uri)
+    album_tracks = album_tracks_result['items'] 
+    track_list = []
+    for album_track in album_tracks:
+        track_list.append(album_track['name'])
+    album_dict[album_name] = track_list
+
+print(album_dict)
