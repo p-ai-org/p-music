35d9dd71bfcbfc8c0563a605a454e5b4d4012cad
MarwanBit <marwanbit12@gmail.com>
2022-11-04 15:41:20 -0700

    Messed with DataSet Generation

---
+++ datapipeline_v1.0.1/spotipykeys.py
@@ -0,0 +1,7 @@
+#Marwan's key's (don't look :0)
+
+keys = {
+    'spotipy_client_id' : 'ea4e6d5d7be74989a45d20e913a07be6',
+    'spotipy_client_secret': '51e991502e594f2f8748bc7a8f23d0a5',
+    'spotipy_redirect_uri' : 'https://www.google.com'
+}
